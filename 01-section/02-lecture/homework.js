5590af9a02c434b2cf2b39d89650a3e6b33b324b
jamik-dev <jamik.oik.group@gmail.com>
2023-02-08 17:52:20 +0500

    Section 1 Lecture 2

---
+++ 01-section/02-lecture/homework.js
@@ -0,0 +1,18 @@
+// # 01 Exercise | Find Area of a rectangle || To'rtburchakning yuzini toping
+// Define a function named "calculateArea" || "calculateArea" nomli funktsiya yarating
+// Within the function, return the area of a rectangle || va funksiyadan to'rtburchak yuzini "return" qiling
+// Invoke the function and pass in two numbers as arguments || funksiyani chaqiring va unga ikkita raqam argument sifatida bering
+// Use console.log to print the result of the function || javobni chiqarish uchun console.log dan foydalaning
+
+// # 02 Exercise | Find Perimeter of a  rectangle || To'rtburchakning peremetrini toping
+// Define a function named "findPerimeter" || "findPerimeter" nomli funktsiya yarating
+// Within the function, return the perimeter of the rectangle || va funksiyadan to'rtburchak peremetri "return" qiling
+// Invoke the "findPerimeter" function and pass in two numbers as arguments || funksiyani chaqiring va unga ikkita raqam argument sifatida bering
+// Use console.log to print the results of the functions || javobni chiqarish uchun console.log dan foydalaning
+
+
+// # 03 Exercise | Find the Third Angle of a Triangle || Uchburchakning 3-chi burchagini toping
+// Define a function named "findThirdAngle" || "findThirdAngle" nomli funktsiya yarating
+// Within the function, return the third angle of the triangle || va funksiyadan uchburchakning 3-chi burchagini "return" qiling
+// Invoke the "findThirdAngle" function and pass in two numbers as arguments || funksiyani chaqiring va unga ikkita raqam argument sifatida bering
+// Use console.log to print the result of the function || javobni chiqarish uchun console.log dan foydalaning
