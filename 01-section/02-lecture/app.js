3a050e7af1ea39c49132089cb5019d2c573a3440
jamik-dev <jamik.oik.group@gmail.com>
2023-02-08 22:58:01 +0500

    Section 1 Lecture 2

---
+++ 01-section/02-lecture/app.js
@@ -0,0 +1,76 @@
+// function myFirstFunction() {
+//   console.log('My first function');
+// };
+
+
+// function greet(name, age) {
+//   let name2 = name;
+//   let age2 = age;
+
+//   console.log(name2, age2);
+//   console.log("Hello, " + name , age , "!");
+
+//   myFirstFunction();
+
+// };
+
+// greet("Ilhom", 19);
+
+
+
+/* let name = "bla";
+let age = 12;
+
+let name2 = name;
+let age2 = age;
+
+console.log(name, age);
+console.log(name2, age2); */
+
+
+
+
+// function multiplyNumbers(a, b) {
+//   let c = a*b;
+//   return c;
+// }
+
+// let result = multiplyNumbers(5, 10);
+// let result2 = multiplyNumbers(7, 11);
+
+
+// result = result * 50;
+
+// result2 = result2 + 11;
+
+
+// console.log(multiplyNumbers(5, 10));
+// // console.log(50);
+// console.log(result);
+// console.log(result2);
+
+
+
+// #1 sum || yig'indi funksiya
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// let result = sum(1, 3);
+
+// console.log(result);
+
+// // console.log(6 + 9)
+// console.log(sum(1, 5) + sum(4, 5));
+
+
+// #2 multiply || ko'paytma
+
+// function multiply(a, b) {
+//   return a*b;
+// }
+
+// let result = multiply(5, 6);
+
+// console.log(result);
