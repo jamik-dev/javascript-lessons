77030fa9d16945d0f3d07e4620f7793b0ab46d31
jamik-dev <jamik.oik.group@gmail.com>
2023-02-12 20:49:31 +0500

    Section 1 Lecture 3

---
+++ 01-section/03-lecture/app.js
@@ -0,0 +1,8 @@
+greeting();
+// sayHello()
+function greeting() {
+  console.log("Hello, World!");
+}
+greeting()
+var sayHello = greeting;
+sayHello(); // Output: Hello, World!
---
+++ 01-section/03-lecture/homework.js
@@ -0,0 +1,8 @@
+// # 1 Exercise | "average" degan funksiya yarating va unga 3 ta number parametr bering
+// Funksiya bizga sonlarning o'rtacha qiymatini qaytarsin
+// Javobni console ga chiqaring
+
+// # 2 Exercise | "averageDouble" degan funksiya yarating va unga 1 ta number parametr bering
+// Funksiyani tepadagi mashqdagi funksiyani ichida chaqiring va parametr sifatida o'rta qiymatni bering
+// Funkisya * 2 ga ko'paytarilgan qiymatini qaytarsin
+// Javobni console ga chiqaring
