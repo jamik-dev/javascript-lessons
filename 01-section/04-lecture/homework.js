a6dc94f6cd1776b33f6ffeb2eece0ee70d41ab78
jamik-dev <jamik.oik.group@gmail.com>
2023-02-14 16:06:59 +0500

    Section 1 Lecture 4

---
+++ 01-section/04-lecture/homework.js
@@ -0,0 +1,9 @@
+// # 1 Exercise | "divider" degan funksiya yarating va unga 2 ta number parametr bering
+// Funksiya bizga birinchi parametrning ikkinchi parametrga bo'linmasini qaytarsin
+// Yangi o'rganilgan operatorlardan foydalanilsin
+// Javobni console ga chiqaring
+
+// # 2 Exercise | "areaOfTriangle" degan funksiya yarating va unga 2 (asos va asosiga tushgan balandlik) ta number parametr bering
+// Funksiyani bizga uchburchakning yuzini qaytarsin
+// Qo'llanma! Uchburchakning yuzini topish uchun, balandlik va asosiga tushgan balandlik ko'paytmasining yarmi topilishi kerak 
+// Javobni console ga chiqaring
