22a7b801c6bc9ffd0a2a7a0c1371df76671a93b0
jamik-dev <jamik.oik.group@gmail.com>
2023-02-18 15:54:22 +0500

    Section 1 Lecure 5

---
+++ 01-section/05-lecture/homework.js
@@ -0,0 +1,8 @@
+// # 1 Exercise | "fruits" degan array yarating, va qiymatlariga "apple", "banana", "orange" bering
+// arrayning ichidagi har bir elementni yangi o'zgaruvchiga olib console da chiqaring
+
+// # 2 Exercise | tepadagi array ga "grape" va "kiwi" qiymatlarini qo'shing
+
+// # 3 Exercise | "person" degan obyekt yarating va qiymatlarini o'zingizning ma'lumotlaringiz bilan to'ldiring
+// name, age, year, job degan key lar bo'lsin
+// har birini console da chiqaring
