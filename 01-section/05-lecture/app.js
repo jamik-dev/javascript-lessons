9b3807e7a93c86165c1666a5add1be500eb4e300
jamik-dev <jamik.oik.group@gmail.com>
2023-02-18 22:46:53 +0500

    Section 1 Lecture 5

---
+++ 01-section/05-lecture/app.js
@@ -0,0 +1,60 @@
+// using array 
+
+// let array1 = [];
+// array1.push("Jamshid1") // push qiymat kiritish
+// array1.push("Jamshid2") // push qiymat kiritish
+// array1.push(18) // push qiymat kiritish
+// array1[10] = "Malohat";
+// console.log(array1[10]);
+// console.log(array1[9]);
+// console.log(array1[2]); 
+
+// let array2 = ["Mahmud", "Kimdir", "Usmon", "Farrux", 1, 5, 7]; // length 7
+// console.log(array2[7]); // undefined
+// let a = array2[1];
+
+// let age;
+// console.log(age)
+
+// creating objects
+
+// let age1 = 18;
+// let year1 = 2004;
+// let age2 = age1;
+
+// let someObj = {
+//   name: "Jamshid",
+//   age: age1,
+//   year: year1
+// };
+
+// console.log(someObj.age);
+
+
+// let name = "Sardor";
+// let age1 = 20;
+
+// let obj1 = {
+//   name: "Jamshid",
+//   age: age1, // age1 bu tepadagi o'zgaruvchi
+//   year: name // name bu tepadagi o'zgaruvchi
+// };
+
+// console.log(obj1.year);
+// console.log(obj1.age);
+
+// let person = {
+//   name: "Jamshid",
+//   age: 18,
+//   gender: "male",
+//   city: "Tashkent"
+// }
+
+// person.name = "Mahmud";
+// person.face = "beautiful";
+
+// let a = 5;
+// let b;
+// b = 10;
+
+// console.log(person.age); // person['age']
