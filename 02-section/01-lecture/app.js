ca72f8c9272e667202d25d26aebef2584ef67fb5
jamik-dev <jamik.oik.group@gmail.com>
2023-02-25 22:18:54 +0500

    Section 2 Lecture 1

---
+++ 02-section/01-lecture/app.js
@@ -0,0 +1,15 @@
+let person = "Jamshid";
+let age = 18;
+let age2 = 19;
+let array1 = [];
+console.log(array1);
+console.log(person);
+
+console.log(person);
+
+let multiplyBothnumbersOfBiningualDigits = 19;
+
+console.log(multiplyBothnumbersOfBiningualDigits);
+
+let age1 = 15;
+console.log(age1);
