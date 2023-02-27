2d17319d6b356d5327a965aebcc052a8ac72d4a5
jamik-dev <jamik.oik.group@gmail.com>
2023-02-27 21:49:02 +0500

    Section 2 Lecture 2

---
+++ 02-section/02-lecture/app.js
@@ -0,0 +1,40 @@
+// let a = '5'; // '5.32'
+// let b = +a;
+
+// console.log(a, b);
+
+// let age = 18;
+// let array1 = [];
+
+// console.log(String(18));
+
+
+// let age = 18;
+// let name = "Jamshid";
+// console.log("1"); // done
+
+// function changeVariable(a, b) {
+//   a = 20;
+//   b = "Sardor";
+//   console.log(c, b) // error
+// }
+
+// console.log("2"); // done
+// console.log(age, name);
+// console.log("3"); // done
+// changeVariable(age, name);
+// console.log("4"); // undone
+
+
+// debuging 
+
+// let age = 18;
+// let name = "Jamshid";
+
+// function changeVariable(a, b) {
+//   a = 20;
+//   b = "Sardor";
+//   console.log(a, b)
+// }
+// console.log(age, name);
+// changeVariable();
