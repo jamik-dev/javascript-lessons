1cde964656ef3aab3b298774cbe7fbb981b288c2
jamik-dev <jamik.oik.group@gmail.com>
2023-03-01 13:29:48 +0500

    Section 3 Lecture 1

---
+++ 03-section/01-lecture/homework.js
@@ -0,0 +1,16 @@
+// # 1 Exercise | "finderNum" degan funksiya yarating va unga 1 ta number parametr bering
+// Funksiya bizga parametrimiz musbat son bo'lsa "Musbat" so'zini qaytarsin
+// Agar manfiy bo'lsa "Manfiy" so'zini qaytarsin
+// Agar 0 soni bo'lsa "Nol" so'zini qaytarsin
+// Agar son bo'lmasa "Raqam emas" so'zini qaytarsin
+// Javobni console ga chiqaring
+
+// # 2 Exercise | "largerNum" degan funksiya yarating va unga 2 ta number bering
+// Funksiya bizga qaysi parametr katta bo'lsa uni string ko'rinishida "a son b sondan ko'ra katta" deb qaytarsin
+// Agar sonlar teng bo'lsa "Sonlar teng" so'zini qaytarsin
+// Javobni console ga chiqaring
+
+// # 3 Exercise | "findOddOrEven" degan funksiya yarating va unga 1 ta number bering
+// Funksiya bizga parametr toq yoki juftligini qaytarsin
+// toq bo'lsa "son toq", just bo'lsa "son juft" deb qaytarsin
+// Javobni console ga chiqaring
