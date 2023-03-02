f1bb620f0744ff46e18e5dadc59646a679f01747
jamik-dev <jamik.oik.group@gmail.com>
2023-03-02 23:39:35 +0500

    Section 3 Lecture 2

---
+++ 03-section/02-lecture/homework.js
@@ -0,0 +1,12 @@
+// # 1 Exercise | "findLargest" degan funksiya yarating va unga 3 ta number parametr bering
+// Funksiya bizga parametrlarimizning eng kattasini qaytarsin
+// Javobni console ga chiqaring
+// (3, 2, 5) => eng kattasi 5 demak "Eng katta son 5"
+
+// # 2 Exercise | "limitFinder" degan funksiya yarating va unga 3 ta number bering
+// Funksiya bizga agar birinchi parametr ikkinchi va uchinchi paramterlar oralig'ida ekanligini (emasligini) tekshirsin
+// Agar sonlar oralig'ida bo'lsa "Oraliqda" deb qaytarsin
+// Agar sonlar oralig'ida bo'lmasa "Oraliqda emas" deb qaytarsin
+// Javobni console ga chiqaring
+// (3, 1, 10) => 3 soni 1 va 10 sonlari oralig'ida demak "Oraliqda"
+// (2, 3, 6) => 2 soni 3 va 6 sonlari oralig'ida emas demak "Oraliqda emas"
