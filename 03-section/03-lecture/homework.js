44d6f41ddbd583b80c1610c30ce7c84f5f588432
jamik-dev <jamik.oik.group@gmail.com>
2023-03-04 16:30:13 +0500

    Section 3 Lecture 3

---
+++ 03-section/03-lecture/homework.js
@@ -0,0 +1,19 @@
+// # 1 Exercise | o'zgaruvchi yarating va unga "age" deb nom bering 
+// o'zgaruvchiga number qiymat bering
+// agar number qiymat 18 dan katta bo'lsa "Siz katta insonsiz", kichik bo'lsa "Siz kichik insonsiz deb chiqsin"
+// ternary operatoridan foydalaning va age ni console da chiqaring 
+
+// # 2 Exercise | o'zgaruvchi yarating va unga "month" deb nom bering 
+// o'zgaruvchiga number qiymat bering
+// switch case orqali month ning qiymati 12 ta oy ning biriga mos kelsa o'sha oyning nomi console da chiqsin
+// let month = 1; => "Yanvar"
+// let month = 10; => "Oktyabr"
+// agar berilgan son 12 holatlarni qanoatlantirmasa "Noto'g'ri oy" deb chiqsin
+
+
+// # 3 Exercise | funksiya yarating va unga "discountGiver" deb nom bering
+// funksiya bizga narxni chegirma bilan hisoblab qaytarsin
+// agar narx 100 dan baland bo'lsa chegirma 20%
+// agar narx 50 va 100 oralig'ida bo'lsa chegirma 15%
+// agar narx 50 dan kichik bo'lsa chegirma 10%
+// agar narx 0 va undan past bo'lsa chegirma yo'q
