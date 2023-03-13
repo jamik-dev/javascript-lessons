9c379d51a360b4c2392a0e668a3604c4e6afe06f
jamik-dev <jamik.oik.group@gmail.com>
2023-03-13 19:11:08 +0500

    Section 3 Lecture 5

---
+++ 03-section/05-lecture/homework.js
@@ -0,0 +1,6 @@
+// # 1 Exercise | for-loop orqali ixtiyoriy son 0.6 dan katta bo'lsa loop'imiz to'xtasin
+
+// # 2 Exercise | for-loop orqali ixtiyoriy son 0.5 dan kichik bo'lsa davom ettirsin
+// agar katta bo'lsa o'sha sonni console da chiqarib loop ni to'xtatsin
+
+// # 3 Exercise | o'zingizdan error yaratib throw qilib koring, va uni try-catch qilib ko'ring
