5f55fb87da9f31388a588c0cdb9b2c59ae5d9ef4
jamik-dev <jamik.oik.group@gmail.com>
2023-03-20 18:38:46 +0500

    Section 5 Lecture 1

---
+++ 05-section/01-lecture/homework.js
@@ -0,0 +1,12 @@
+// # 1 Exercise | funksiya (expression) yarating va unga parametr sifatida array bering
+// arrayda xohlagancha son bo'lsin
+// funksiya bizlarga o'sha sonlarning yig'indisini qaytarsin
+// javobni console da chiqaring
+
+// # 2 Exercise | funksiya (decloration) yarating va unga xohlagan nomingizni bering
+// funksiya bizga 5ta 0.6 dan katta bo'lgan ixtiyoriy sonlar to'plamini qaytarsin
+// javobni console da chiqaring
+
+// # 3 Exercise | setTimeout funksiyasi orqali 
+// har 2 sekundda "hello" so'zini yozadigan funksiya yarating
+// "hello" so'zi 5 martta ishlagandan so'ng to'xtasin
