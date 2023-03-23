2007f2a6ea5685b1da0711a375d821063adec687
jamik-dev <jamik.oik.group@gmail.com>
2023-03-23 23:47:33 +0500

    Section 5 Lecture 3

---
+++ 05-section/03-lecture/homework.js
@@ -0,0 +1,13 @@
+// # 1 Exercise | arrow funksiyaga xos 5 ta sifatning barchasiga misol yozing
+
+// # 2 Exercise | funksiya yarating va unga 2ta parametr bering
+// birinchi parametrga ism
+// ikkinchi paramterga familya
+// agar familya'ga argument berish unutilsa, standard holatda "Surname" string berilsin
+// console da ism va familyani qatnashtirib gap tuzing 
+
+// # 3 Exercise |  funksiya yarating va unga ixtiyoriy miqdorda parametr bera oladigan qiling
+// parametrlar barchasi number bo'lishi kerak
+// funksiya bizga parametrlarning o'rtacha qiymatini qaytarsin
+// agar bironta parametr string bo'lib qolsa uni olmay ketsin
+// javobni console da chiqaring
