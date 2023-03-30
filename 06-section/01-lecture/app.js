6ef8abd80e893f061de61c0c3633c948e5d4cd83
jamik-dev <jamik.oik.group@gmail.com>
2023-03-30 06:58:17 +0500

    Section 6 Lecture 1

---
+++ 06-section/01-lecture/app.js
@@ -0,0 +1,3 @@
+console.log(document.body) //=> <body> element node ni tanlaydi  
+console.log(document.head) //=> <head> element node ni tanlaydi  
+console.log(document.documentElement) //=> <html> element node ni tanlaydi
