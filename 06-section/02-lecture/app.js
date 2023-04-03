d75c865b16c28c1971ab28a381ca361ce3dd0133
jamik-dev <jamik.oik.group@gmail.com>
2023-04-03 07:08:41 +0500

    Section 6 Lecture 2

--- 06-section/02-lecture/app.js
+++ 06-section/02-lecture/app.js
@@ -1,3 +1,17 @@
-console.log(document.body) //=> <body> element node ni tanlaydi  
-console.log(document.head) //=> <head> element node ni tanlaydi  
-console.log(document.documentElement) //=> <html> element node ni tanlaydi
+// const listItemELements = document.querySelectorAll('li'); // not live change #1
+// const listItemElements = document.getElementsByTagName('li'); // live change #2
+
+// for (const listItemEl of listItemElements) {
+//   console.dir(listItemEl); 
+// }
+
+// const h1 = document.getElementById('main-title');
+// h1.textContent = 'Some new title!';
+// h1.style.color = 'red';
+// h1.style.backgroundColor = 'black';
+
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + ' (Changed!)';
+
+// const div = document.querySelector('div');
+// const h1 = div.querySelector('.className');
